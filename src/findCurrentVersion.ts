b51749da817028196dd70775cfea0cb1f306944b
Amir Yonatan <amiry@wix.com>
2022-02-23 19:32:39 +0200

    supporting monorepo

--- src/findCurrentVersion.ts
+++ src/findCurrentVersion.ts
@@ -4,7 +4,7 @@ export function findCurrentVersion(packageName) {
   let version;
 
   try {
-    const path = './node_modules/' + packageName + '/package.json';
+    const path = require.resolve(packageName + '/package.json');
     const packageContent = fs.readFileSync(path, 'utf-8');
     version = JSON.parse(packageContent).version;
   } catch (e) {
